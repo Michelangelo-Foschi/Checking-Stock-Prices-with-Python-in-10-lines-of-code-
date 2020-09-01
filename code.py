169af80a5d8817ffe027882d3feeb127ea696950
Michelangelo Foschi <michelangelo.foschi.10@gmail.com>
2020-09-01 18:00:45 +0400

    Final code.py
    Notice: url is of your own choice. (Tip: Use yahoo finance for a faster data retrieval)

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
-
+import requests
+from bs4 import BeautifulSoup
+def check_price():
+    url = 'https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch'
+    get_url_response = requests.get(url)
+    parse_response = BeautifulSoup(get_url_response.text, 'lxml')
+    price = parse_response.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+    print(price)
+while True:
+    check_price()
